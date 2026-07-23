dd27a76168c9324a60546f9b2799808c4a9b375e
catac1 <catac1@users.noreply.github.com>
2026-07-23 15:03:07 +0900

    feat: mqtt test

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routers.upload import router as upload_router
+from app.routers.mqtt import router as mqtt_router
 
 app = FastAPI()
 app.include_router(upload_router)
+app.include_router(mqtt_router)
